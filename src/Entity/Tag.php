c85cb4271b4bcb7a5ecb5ac50f02c195e5bfaf3d
guham <guham.dev@gmail.com>
2018-03-06 12:09:58 -0600

    Marked Tag as mapped super class

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -7,12 +7,10 @@ namespace Guham\TaskManagerBundle\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity()
- * @UniqueEntity("name")
+ * @ORM\MappedSuperclass()
  */
 class Tag
 {
