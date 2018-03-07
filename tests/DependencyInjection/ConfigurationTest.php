148ea655f0c9e0dad4a0f44733cda85078fec22f
guham <guham.dev@gmail.com>
2018-03-06 18:39:29 -0600

    Added entities class configuration

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -28,7 +28,7 @@ class ConfigurationTest extends TestCase
         $this->processor = new Processor();
     }
 
-    public function testDefaultConfig()
+    public function testDefaultConfig(): void
     {
         $treeBuilder = $this->configuration->getConfigTreeBuilder();
         $config = $this->processor->processConfiguration($this->configuration, []);
@@ -37,6 +37,57 @@ class ConfigurationTest extends TestCase
         $this->assertInstanceOf(ConfigurationInterface::class, $this->configuration);
         $this->assertEquals([
             'homepage_route' => '/',
+            'entities' => [
+                'Task' => [
+                    'class' => '',
+                ],
+                'Tag' => [
+                    'class' => '',
+                ],
+            ],
         ], $config);
+    }
+
+    /**
+     * @dataProvider invalidEntityClassValueProvider
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     */
+    public function testExceptionToConfigWithInvalidEntityClass($invalidEntityClass): void
+    {
+        $this->processor->processConfiguration($this->configuration, [
+            'guham_task_manager' => [
+                'entities' => [
+                    'Task' => [
+                        'class' => $invalidEntityClass,
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Config\Definition\Exception\InvalidConfigurationException
+     * @expectedExceptionMessageRegExp /The class ".+" does not exist./
+     */
+    public function testExceptionMessageToConfigWithInvalidEntityClass(): void
+    {
+        $this->processor->processConfiguration($this->configuration, [
+            'guham_task_manager' => [
+                'entities' => [
+                    'Task' => [
+                        'class' => 'Foo',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function invalidEntityClassValueProvider(): array
+    {
+        return [
+            [null],
+            [''],
+            ['App\EntityDoesNotExist\Foo'],
+        ];
     }
 }
